8208d7154ab6c6c52955e714dfb92b4d0c88c0ae
Giuliano Belinassi <gbelinassi@suse.de>
2022-05-12 18:00:25 -0300

    Add livepatchability check on build process
    Some repositories on OBS and IBS do not contain a livepatchable glibc.
    Therefore, check it when building.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

--- pthread_mutex_unlock/libpthread_livepatch1.c
+++ pthread_mutex_unlock/libpthread_livepatch1.c
@@ -13,7 +13,6 @@ int pthread_mutex_unlock_lp(pthread_mutex_t *lock)
 {
   func_t *func_ptr = pthread_mutex_unlock;
   func_ptr = skip_ulp_redirect_insns(func_ptr);
-  printf("livepatched: in pthread_mutex_unlock_lp\n");
 
   int ret = func_ptr(lock);
   return ret;
